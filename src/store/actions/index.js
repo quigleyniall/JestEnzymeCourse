8bf50c2194b3360cc5dc1b808d16a2cf0380d7cf
Niall Quigley <niallquigley92@gmail.com>
2019-11-05 18:18:44 +0000

    Test async action creator

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import { getLetterMatchCount } from '../../helpers'
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
-  GUESS_WORD: 'GUESS_WORD'
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD'
 };
 
 export const guessWord = (guessedWord) => (dispatch, getState) => {
@@ -17,4 +19,14 @@ export const guessWord = (guessedWord) => (dispatch, getState) => {
   if (guessedWord === secretWord) {
     dispatch({ type: actionTypes.CORRECT_GUESS })
   }
+}
+
+export const getSecretWord = () => (dispatch) => {
+  return axios.get('http://localhost:3030')
+  .then((res) => {
+    dispatch({
+      type: actionTypes.SET_SECRET_WORD,
+      payload: res.data
+    });
+  })
 }
--- src/store/reducers/secretWordReducer.js
+++ src/store/reducers/secretWordReducer.js
@@ -1,5 +1,9 @@
+import { actionTypes } from "../actions";
+
 const secretWord = (state = null, action) => {
   switch (action.type) {
+    case actionTypes.SET_SECRET_WORD:
+      return action.payload
     default:
       return state;
   }
