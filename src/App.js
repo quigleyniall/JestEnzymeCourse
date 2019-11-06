da15d9cb9b198cf1a33fdfbc5e663825244b5208
Niall Quigley <niallquigley92@gmail.com>
2019-11-06 16:23:58 +0000

    Connect app component (and tests)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { getSecretWord } from './store/actions';
 import Congrats from './components/Congrats/Congrats';
-import './App.css';
 import GuessedWords from './components/GuessedWords/GuessedWord';
 import Input from './components/Input/Input';
+import './App.css';
 
-function App() {
-  const guessedWords = [
-    { guessedWord: 'train', letterMatchCount: 3 },
-    { guessedWord: 'agile', letterMatchCount: 1 },
-    { guessedWord: 'party', letterMatchCount: 5 },
-  ];
-  return (
-    <div className="container">
-      <h3>Jotto</h3>
-      <Congrats success={true} />
-      <Input />
-      <GuessedWords guessedWords={guessedWords} />
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    const { getSecretWord } = this.props;
+    const secretWord = getSecretWord();
+  }
+  render() {
+    const { success, guessedWords } = this.props;
+    return (
+      <div className="container">
+        <h3>Jotto</h3>
+        <Congrats success={success} />
+        <Input />
+        <GuessedWords guessedWords={guessedWords} />
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = ({ success, guessedWords, secretWord }) => ({ success, guessedWords, secretWord })
+
+export default connect(mapStateToProps, { getSecretWord })(App);
